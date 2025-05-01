80b544fef4c163c473ee1c51a6e94ac394c01d96
Asad Basha <145152174+Asadcodeing@users.noreply.github.com>
2025-05-01 16:22:38 +0530

    Add files via upload
    A simple and interactive number guessing game built with HTML, CSS, and JavaScript. Guess a number between 1 and 10 with only 3 chances!

---
+++ script.js
@@ -0,0 +1,28 @@
+const secreteNum = Math.floor(Math.random() * 10) + 1;
+let attempts = 3;
+
+function numGuess() {
+    const num = parseInt(document.getElementById("userGuess").value);
+    const result = document.getElementById("result");
+    const attemptsLeft = document.getElementById("attemptsLeft");
+
+    if(isNaN(num) || num < 1 || num > 10){
+        result.textContent = "InValid Input! Guess number between 1 and 10";
+        attemptsLeft.textContent = "";
+    }
+
+    if(num === secreteNum){
+        result.textContent = "Congratulations! You won the Game";
+        attemptsLeft.textContent = "";
+    }else{
+        attempts--;
+        if(attempts > 0){
+            result.textContent = num > secreteNum ? "Too High" : "Too Low";
+            attemptsLeft.textContent = `Guess correct number. Now! You have only ${attempts} attempts`;
+        }else{
+            result.textContent = `Game Over! You lose the Game. Secerate number is ${secreteNum}. Refresh the Website and Play Again`;
+            attemptsLeft.textContent = "";
+        }
+    }
+
+}
